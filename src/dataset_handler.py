a802264710a7a83f16422078e7398f2f3a38e7be
Tien Nguyen Anh <tien.nguyenanh94@gmail.com>
2022-05-29 13:24:02 +0900

    init code

--- src/dataset_handler.py
+++ src/dataset_handler.py
@@ -11,15 +11,18 @@ from sklearn.model_selection import train_test_split
 import utils
 
 class DatasetHandler(object):
-    def __init__(self, dataset, target_column):
+    def __init__(self, dataset, target_column, id_column):
         """
         @args:
             df: Pandas Data Frame
         """
         self.df = utils.read_csv(dataset)
         self.target_column = target_column
+        self.id_column = None
 
     def parse(self):
+        if self.id_column != None:
+            df.drop(self.id_column, axis=1, inplace=True)
         df = self.df.copy()
         labels = list(df[self.target_column])
         df.drop(self.target_column, axis=1, inplace=True)
