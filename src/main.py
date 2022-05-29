2823a9af39e21a30a0b7b4476e3a5c7b0e2b1f13
Tien Nguyen Anh <tien.nguyenanh94@gmail.com>
2022-05-29 17:05:46 +0900

    imputation using mean

--- src/main.py
+++ src/main.py
@@ -14,5 +14,12 @@ def train_model(dataset, target, id_column=None):
     train_acc, val_acc = classifier.run()
     print(f"TRAIN ACC: {train_acc} - VAL ACC: {val_acc}")
 
+def mock_data(dataset, target_column, mock_file_name):
+    mocker = Mocker()
+    df_mock = mocker.mock(dataset, target_column)
+    utils.dump_csv(df_mock, mock_file_name)
+
 if __name__ == '__main__':
-    train_model(configs.MOCK_PARKS, configs.PARKS_TARGET, configs.PARKS_ID_COLUMN)
+    # train_model(configs.MOCK_PARKS, configs.PARKS_TARGET, configs.PARKS_ID_COLUMN)
+    mock_data(configs.PIDD, configs.PIDD_TARGET, configs.MOCK_PIDD)
+    train_model(configs.MOCK_PIDD, configs.PIDD_TARGET)
