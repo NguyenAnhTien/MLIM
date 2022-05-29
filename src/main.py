06283adaf7983e3431edf7af0ac331854ca7404f
Tien Nguyen Anh <tien.nguyenanh94@gmail.com>
2022-05-29 17:27:21 +0900

    imputation using mean

--- src/main.py
+++ src/main.py
@@ -20,6 +20,7 @@ def mock_data(dataset, target_column, mock_file_name):
     utils.dump_csv(df_mock, mock_file_name)
 
 if __name__ == '__main__':
+    utils.set_seed()
     # train_model(configs.MOCK_PARKS, configs.PARKS_TARGET, configs.PARKS_ID_COLUMN)
     # mock_data(configs.PIDD, configs.PIDD_TARGET, configs.MOCK_PIDD)
     train_model(configs.PIDD, configs.PIDD_TARGET)
